9ebc53732cc5205388b1b341a059925e0813111b
Adam Olson <adajolson@yahoo.com>
2023-06-07 14:00:32 -0600

    initial fixtures/dummy data

--- remotefanapi/models/bar.py
+++ remotefanapi/models/bar.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Bar(models.Model):
 
     name = models.CharField(max_length=75)
     city = models.ForeignKey("City", on_delete=models.CASCADE)
-    owner = models.ForeignKey("User", on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     teams = models.ManyToManyField("Team", related_name="barTeams")
