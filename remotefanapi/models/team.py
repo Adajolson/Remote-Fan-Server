169906192cea222c6c81a90f0cc72afe3ec4e58f
Adam Olson <adajolson@yahoo.com>
2023-06-13 23:04:45 -0600

    update and custom decorator

--- remotefanapi/models/team.py
+++ remotefanapi/models/team.py
@@ -5,3 +5,4 @@ class Team(models.Model):
     name = models.CharField(max_length=50)
     city = models.ForeignKey("City", on_delete=models.CASCADE)
     sport = models.ForeignKey("Sport", on_delete=models.CASCADE)
+    bars = models.ManyToManyField("Bar", related_name="teamBars")
